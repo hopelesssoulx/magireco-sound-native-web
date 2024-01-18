5866fc42390fc69b401c77c7f456d894023340ad
hopelesssoulx <hopelesssoulx@gmail.com>
2024-01-18 20:58:38 +0800

    初步

--- src/views/index.vue
+++ src/views/index.vue
@@ -2,15 +2,19 @@
   <div class="index">
     <el-tabs v-model="activeTabName" @tab-click="tabClick">
       <el-tab-pane
+        v-for="(listDetail, index) in list"
         :label="index"
         :name="index"
-        v-for="(listDetail, index) in list"
       >
         <div
           v-for="(item, index2) in listDetail"
-          @click="fileClick(index + '/' + item)"
+          @click="fileClick(index + '/' + item.name)"
+          class="flex-left"
         >
-          {{ item }}
+          <span class="pd-10"> {{ index2 }}</span>
+          <span class="pd-10"> {{ item.name }}</span>
+          <span class="pd-10"> <audio controls :src="audioFile"></audio></span>
+          <span class="pd-10"> {{ item.character }}</span>
         </div>
       </el-tab-pane>
     </el-tabs>
@@ -19,11 +23,16 @@
 
 <script>
 import * as commonApi from "../api/common.js";
+import { hcaStr } from "../utils/hcaStr";
 export default {
   data() {
     return {
       list: [],
       activeTabName: "",
+      audioFile: "",
+      fileName: "",
+
+      hcaJsUrl: new URL(hcaStr, document.baseURI),
     };
   },
   created() {
@@ -35,7 +44,7 @@ export default {
       that.getFile(path);
     },
     tabClick(tab, event) {
-      console.log(tab, event);
+      let that = this;
     },
     getList() {
       let that = this;
@@ -43,19 +52,51 @@ export default {
         that.list = res.data;
       });
     },
-    getFile(path) {
+    async getFile(path) {
       let that = this;
-      commonApi.getFile(path).then((res) => {
-        console.log(res);
+      let res = await fetch("http://127.0.0.1:16168/getFile/" + path);
+      that.decryptAndDecode(res);
+    },
+    async decryptAndDecode(hca) {
+      let that = this;
+      let res = await fetch(that.hcaJsUrl.href);
+      let blob = new Blob([await res.arrayBuffer()], {
+        type: "text/javascript",
       });
+      let hcaJsObjUrl = URL.createObjectURL(blob);
+      let hcaJsModule = await import(hcaJsObjUrl);
+      let HCAWorker = hcaJsModule.HCAWorker;
+      let worker = await HCAWorker.create(hcaJsObjUrl);
+
+      let ab = await hca.arrayBuffer();
+      let buffer = new Uint8Array(ab);
+
+      let decrypted = await worker.decrypt(
+        buffer.slice(0),
+        "0x01395C51",
+        "0x00000000"
+      );
+      let wav = await worker.decode(decrypted, 16);
+      await worker.shutdown();
+
+      let tmpUrl = URL.createObjectURL(
+        new Blob([wav], { type: "audio/x-wav" })
+      );
+
+      that.audioFile = tmpUrl;
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.card {
-  padding: 20px;
-  background-color: #f8f8f8;
+.flex-left {
+  display: flex;
+  justify-content: left;
+  align-items: center;
+}
+
+.pd-10 {
+  padding: 10px;
 }
 </style>
